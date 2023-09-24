18dbfb66e3a272fc6852a7b9bd319c37197c5901
AQR88 <timahaua@gmail.com>
2023-09-24 14:27:53 +0300

    task_8

--- js/task-08.js
+++ js/task-08.js
@@ -1,4 +1,19 @@
-const form = document.querySelector('.login-form');
-console.log(form);
+const form = document.querySelector(".login-form");
 
-        
+form.addEventListener("submit", acceptFoo);
+
+function acceptFoo(evt) {
+  evt.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = evt.currentTarget;
+
+  if (email.value && password.value !== "") {
+    console.log(`email: ${email.value}`, `\n\npassword:${password.value}`);
+  } else {
+    alert(`Потрібно заповнити всі поля`);
+  }
+  form.reset();
+}
+
