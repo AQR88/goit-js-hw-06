923e28392563fbe328efa47671a21497abcdef94
AQR88 <timahaua@gmail.com>
2023-09-24 15:14:25 +0300

    task_9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const body = document.querySelector('body')
+const btn = document.querySelector('.change-color');
+let spanTxt = document.querySelector('.color');
+// console.log(body);
+
+btn.addEventListener('click', colorChange);
+
+function colorChange(){
+  const result = getRandomHexColor();
+  spanTxt.textContent = result;
+  body.style.backgroundColor = getRandomHexColor();
+  console.log(result);
+}
+
